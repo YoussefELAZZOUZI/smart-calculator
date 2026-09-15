3a5be9aa6d03f23f2c5f90cbb2f484e3a80b1d4c
youssefELAZZOUZI <youssefelazzouzi010@gmail.com>
2026-09-15 17:40:30 +0100

    Build calculator with validation

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
 <?php 
-echo "hello word";
-echo "Welcome to my first project";
+
+if (isset($_POST['number1']) && isset($_POST['number2'])){
+    $number1Input = trim($_POST['number1']);
+    $number2Input = trim($_POST['number2']);
+    if ($number1Input === '' || $number2Input === '') {
+        echo "Both numbers are required" . "<br>";
+    } else {
+        $number1 = (float) $number1Input;
+        $number2 = (float) $number2Input;
+        $operator = $_POST['operator'];
+        $allowedOperators = ['+', '-', '*', '/'];
+
+        if (!in_array($operator, $allowedOperators, true)) {
+            echo "Invalid operator";
+        } else {
+            if ($operator === "+"){
+                echo $number1 + $number2;
+            } elseif ($operator === "-"){
+                echo $number1 - $number2;
+            } elseif ($operator === "*"){
+                echo $number1 * $number2;
+            } elseif ($operator === "/"){
+                if($number2 === 0.0){
+                    echo "cannot divide by zero";
+                } else {
+                    echo $number1 / $number2;
+                }
+            }
+        }
+    }
+}
 ?>
+
+<form method="post">
+    <input type="number" name="number1" step="any">
+    <input type="number" name="number2" step="any">
+
+    <select name="operator" >
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <button type="submit">Calculate</button>
+</form>
